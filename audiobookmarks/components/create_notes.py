23d6c8dff3c8ed9c70b72d4331f665085abb264f
Brandon Wilde <brandon.t.wilde@gmail.com>
2024-09-23 12:23:24 -0500

    Fix formatting in note writing

--- audiobookmarks/components/create_notes.py
+++ audiobookmarks/components/create_notes.py
@@ -11,10 +11,10 @@ def write_notes(bookmarks_data, notes_dir):
     notes_file = os.path.join(notes_dir, f"{title}.md")
     with open(notes_file, "w") as f:
         f.write("---\n")
-        f.write(f'Author: "[[{author.translate(str.maketrans('','',' .'))}]]"\n')
+        f.write(f'''Author: "[[{author.translate(str.maketrans('','','.'))}]]"\n''')
         f.write("---\n")    
         for bookmark in bookmarks:
-            f.write(f"### Bookmark {bookmark['bookmark_num']} (Chapter {bookmark['chapter_num']}, {bookmark['minutes_in']} in)\n")
+            f.write(f"### Bookmark {bookmark['bookmark_num']} ({bookmark['chapter_num']}, {bookmark['minutes_in']} in)\n")
             if 'note' in bookmark:
                 f.write(f"#### {bookmark['note']}\n")
             f.write(f"{bookmark['quote']}\n\n")
