5497731f0146c828c58fe480dcda4d05d5d7d19e
Brandon Wilde <brandon.t.wilde@gmail.com>
2024-10-09 21:21:06 -0500

    Organize Libby bookmarks by chapter

--- audiobookmarks/libby/create_notes.py
+++ audiobookmarks/libby/create_notes.py
@@ -12,13 +12,19 @@ def write_notes(bookmarks_data, notes_dir):
     with open(notes_file, "w") as f:
         f.write("---\n")
         f.write(f'''Author: "[[{author.translate(str.maketrans('','','.'))}]]"\n''')
-        f.write("---\n")    
+        f.write("---\n")
+        # We assume the bookmarks are in order
+        current_chapter = ''
         for bookmark in bookmarks:
-            f.write(f"### Bookmark {bookmark['bookmark_num']} ({bookmark['chapter_num']}, {bookmark['minutes_in']} in)\n")
+            if bookmark['chapter_num'] != current_chapter:
+                current_chapter = bookmark['chapter_num']
+                f.write(f"\n---\n\n")
+                f.write(f"## {current_chapter}\n")
+            f.write(f"#### Bookmark {bookmark['bookmark_num']} *({bookmark['minutes_in']} in)*\n")
             if 'note' in bookmark:
-                f.write(f"#### {bookmark['note']}\n")
-            f.write(f"{bookmark['quote']}\n\n")
-            f.write(f"---\n\n")
+                f.write(f"**{bookmark['note']}**\n")
+            f.write(f"{bookmark['quote']}\n")
+            
     
     print(f"Notes written to {notes_file}")
     return
