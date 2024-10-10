52870c2cdf0091a0670a1e215572f88aab3625fd
Brandon Wilde <brandon.t.wilde@gmail.com>
2024-10-10 13:35:39 -0500

    Add model for args, generate argparser automatically

---
+++ audiobookmarks/utils.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, PydanticUndefined
+from argparse import ArgumentParser
+
+def generate_arg_parser(model: BaseModel) -> ArgumentParser:
+    '''
+    Generate an argparse.ArgumentParser from a Pydantic model of the same fields.
+    The model config must include a description attribute. This will be used to
+    describe the function when the help flag is used.
+    '''
+    parser = ArgumentParser(description=model.model_config['description'])
+    for name, field in model.model_fields.items():
+        field_type = field.annotation
+        help_text = field.description
+        default_value = field.default if field.default is not PydanticUndefined else None
+
+        if field_type == bool:
+            parser.add_argument(
+                f'--{name}',
+                action='store_true',
+                help=f'{help_text}' + (f' (default: {default_value})' if default_value is not None else ''),
+                )
+        else:
+            parser.add_argument(
+                name,
+                type=field_type,
+                help=f'{help_text}' + (f' (default: {default_value})' if default_value is not None else ''),
+                default=default_value
+            )
+    return parser
--- main.py
+++ main.py
@@ -1,33 +1,46 @@
-import argparse
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+from audiobookmarks.utils import generate_arg_parser
 load_dotenv(override=True)
 
 from audiobookmarks.main_hoopla import main as hoopla_main
 from audiobookmarks.main_libby import main as libby_main
 
-book_name = "In Love With The World"
-platform = "libby"
-
 # If running in debug mode, first run the following command in the terminal:
 # google-chrome --remote-debugging-port=9222
 
-def main():
-    parser = argparse.ArgumentParser(description='Get bookmarks for an audiobook.')
-    parser.add_argument('platform', type=str, help='The platform hosting the book and bookmarks.')
-    parser.add_argument('book', type=str, help='The name of the audiobook.')
-    parser.add_argument("--debug", action="store_true", help="Run browser in debug mode (not headless).")
-    args = parser.parse_args()
 
+class Args(BaseModel):
+    '''
+    Args for the main audiobookmarks function.
+    '''
+    platform: str = Field(..., description="The platform hosting the book and bookmarks.")
+    book: str = Field(..., description="The name of the audiobook.")
+    debug: bool = Field(False, description="Run browser in debug mode (not headless).")
+
+    class Config:
+        description = "Get bookmarks for an audiobook"
+
+
+def main(args: Args):
+    '''
+    Get bookmarks for an audiobook.
+    '''
     if args.debug:
         os.environ["DEBUG_MODE"] = "true"
     else:
         os.environ["DEBUG_MODE"] = "false"
-
+    
     if args.platform == "libby":
         libby_main(args.book)
     elif args.platform == "hoopla":
         hoopla_main(args.book)
 
 if __name__ == "__main__":
-    main()
+    parser = generate_arg_parser(Args)
+    args = parser.parse_args()
+
+    args_obj = Args(**vars(args))
+    main(args_obj)
