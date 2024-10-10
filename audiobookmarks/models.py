5bfdfad1fdac01b47e9625863ed40f9857183f43
Brandon Wilde <brandon.t.wilde@gmail.com>
2024-10-09 20:37:14 -0500

    Fix misnamed var in Libby model

--- audiobookmarks/models.py
+++ audiobookmarks/models.py
@@ -6,8 +6,8 @@ class LibbyBookDataTree:
         self.title = book_name
         self.file_name = book_name.replace(' ', '_').lower()
         self.dir = os.path.join(self.root, self.file_name)
-        self.audio_dir = os.path.join(self.file_dir, "audio")
-        self.file = os.path.join(self.file_dir, self.file_name + '.json')
+        self.audio_dir = os.path.join(self.dir, "audio")
+        self.file = os.path.join(self.dir, self.file_name + '.json')
         self.updated_file = self.file.replace(".json", "_updated.json")
 
 class HooplaBookDataTree:
